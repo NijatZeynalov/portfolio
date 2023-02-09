09b8b9588ca18342c35bd2e2dadcd8f6fc8d7828
Nijat Zeynalov <31247506+NijatZeynalov@users.noreply.github.com>
2023-02-09 23:40:12 +0400

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ export default function FourZeroFour() {
     <div className='fade-in flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6'>
       <div className='space-x-2 pt-6 pb-8 md:space-y-5'>
         <h1 className='text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14'>
-          NO ACCESS
+          403
         </h1>
       </div>
       <div className='max-w-md'>
@@ -14,6 +14,8 @@ export default function FourZeroFour() {
         </p>
         <p className='mb-8'>
           Lessons, supplemental materials, assignments, projects, and other resources will only be shared with students in live classes.
+          
+          Please contact me via linkedin.
         </p>
         <Link href='/'>
           <button className='focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500'>
