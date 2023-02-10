df675dac4e1084683e03ee12fb83672c8aac2069
Nijat Zeynalov <31247506+NijatZeynalov@users.noreply.github.com>
2023-02-10 20:43:08 +0400

    Update courses.ts

--- config/courses.ts
+++ config/courses.ts
@@ -31,6 +31,20 @@ export const courses: Course[] = [
     {
         name: 'Getting Started',
         content: [
+                    {
+            name: 'Machine Learning with Python',
+            slug: 'what-is-system-design',
+          },
+                    {
+            name: 'Deep Learning with TensorFlow',
+            slug: 'what-is-system-design',
+          },
+                    },
+                    {
+            name: 'AWS for Machine Learning',
+            slug: 'what-is-aws-course',
+          },
+          
           {
             name: 'What will you get from these courses?',
             slug: 'what-is-system-design',
